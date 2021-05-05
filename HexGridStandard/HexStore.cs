11034e6587e55613eec382f64289ee1f4b8652e5
Ivan Zverev <ivan.zverev@gmail.com>
2021-05-04 18:38:46 -0700

    -some optimisations for more performant copying.

--- HexGridStandard/HexStore.cs
+++ HexGridStandard/HexStore.cs
@@ -6,26 +6,43 @@ namespace HexGridStandard
 {
     public class HexStore<TKey, TValue>
     {
-        public Dictionary<TKey, TValue> _store {get;set; }
+        List<TValue> _values;
+        Dictionary<TKey, int> _keys;
 
-        public int Count => _store.Count;
+        public int Count => _values.Count;
 
-        public TValue this[TKey key] => _store[key];
+        public TValue this[TKey key] => _values[_keys[key]];
+
+        public TValue this[int iter] => _values[iter];
 
         public HexStore()
         {
-             _store = new Dictionary<TKey, TValue>();
+            _values = new List<TValue>();
+            _keys = new Dictionary<TKey, int>();
         }
 
+        public IEnumerable<TValue> All()
+        {
+	        for(int i = 0; i < _values.Count; i++) 
+                yield return _values[i];	        
+        }
         public HexStore(int capacity)
         {
-            _store = new Dictionary<TKey, TValue>(capacity);
+            _values = new List<TValue>(capacity);
+            _keys = new Dictionary<TKey, int>(capacity);
         }
         public void Remove(TKey pos, Func<TKey, bool> removal)
         {
-            if (!_store.TryGetValue(pos, out _) || !removal(pos))
+            if (!_keys.TryGetValue(pos, out _) || !removal(pos))
                 return;
-            _store.Remove(pos);
+            var position = _keys[pos];
+            _keys.Remove(pos);
+            foreach(var key in _keys.Keys)
+            {
+                if(_keys[key] > position)
+                    _keys[key]--;
+            }
+            _values.RemoveAt(position);
         }
 
         public TValue Add(
@@ -33,18 +50,27 @@ namespace HexGridStandard
           Action<TValue> duplicateProcess,
           Func<TKey, TValue> creationProcess)
         {
-            if (_store.ContainsKey(pos))
-                duplicateProcess(_store[pos]);
+            if (_keys.ContainsKey(pos))
+                duplicateProcess(_values[_keys[pos]]);
             else
-                _store.Add(pos, creationProcess(pos));
-            return _store[pos];
+            {
+                _values.Add(creationProcess(pos));
+                _keys.Add(pos, _values.Count-1);
+            }
+            return _values[_values.Count-1];
         }
 
         internal void Clear(Action<TValue> valueCleanup)
         {
-            foreach (var key in _store.Keys)
-                valueCleanup(_store[key]);
-            _store.Clear();
+            foreach (var key in _keys.Keys)
+                valueCleanup(_values[_keys[key]]);
+            _values.Clear();
+            _keys.Clear();
+        }
+
+        public bool ContainsKey(TKey position)
+        {
+            return _keys.ContainsKey(position);
         }
     }
 }
---
+++ HexGridStandardTests/HexStoreTests.cs
@@ -0,0 +1,36 @@
+﻿using HexGridStandard;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HexGridStandardTests
+{
+    public class HexStoreTests
+    {
+        [Test]
+        public void CanUpdateValuesOnTilesInHexStoreAfterCallingAll()
+        {
+            var hexStore = new HexStore<Vector3, TestTile>();
+
+            hexStore.Add(new Vector3(0,0,0), (x)=> throw new Exception(), (x)=>new TestTile());
+            hexStore.Add(new Vector3(0,1,0), (x)=> throw new Exception(), (x)=>new TestTile());
+            hexStore.Add(new Vector3(0,1,1), (x)=> throw new Exception(), (x)=>new TestTile());
+            hexStore.Add(new Vector3(1,1,1), (x)=> throw new Exception(), (x)=>new TestTile());
+
+            for(var i = 0; i < hexStore.Count;i++)
+                hexStore[i].ValueA = i+1;
+            
+            Assert.AreEqual(1, hexStore[0].ValueA);
+            Assert.AreEqual(2, hexStore[1].ValueA);
+            Assert.AreEqual(3, hexStore[2].ValueA);
+            Assert.AreEqual(4, hexStore[3].ValueA);
+
+        }
+    }
+
+    public class TestTile
+    {
+        public int ValueA {get;set;}
+    }
+}
